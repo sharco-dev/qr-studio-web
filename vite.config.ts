42f2fa60ce5c30459b386947a2e24dcd722b8bfc
sharco <marcantodev20@hotmail.com>
2026-05-20 21:27:23 -0600

    add: GitHub Pages config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/qr-studio-web/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
